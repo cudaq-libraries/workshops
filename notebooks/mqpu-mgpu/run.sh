826542f86ae13ce6697e00a5926905ef42725842
ikkoham <ikkoham@users.noreply.github.com>
2024-10-21 12:08:30 +0900

    update mqpu-mgpu

--- notebooks/mqpu-mgpu/run.sh
+++ notebooks/mqpu-mgpu/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #PJM -L jobenv=singularity
-#PJM -L rscgrp=cx-debug
+#PJM -L rscgrp=b-batch
 #PJM -L node=1
+#PJM -L elapse=1:00:00
 #PJM -j
 
-module load singularity
+module load singularity-ce
 singularity exec --nv docker://nvcr.io/nvidia/quantum/cuda-quantum:0.8.0 mpirun -np 4 python ghz.py --target nvidia --target-option mgpu

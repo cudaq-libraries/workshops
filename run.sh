7a502dee723bc9e0281671e2d1b39c59b1cd1d87
ikkoham <ikkoham@users.noreply.github.com>
2024-12-17 11:40:08 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PJM -g <project-code>
+#PJM -L rscgrp=tutorial-a
+#PJM -L gpu=1
+#PJM -L elapse=00:05:00
+
+module load cudaq
+source $MINICONDA_DIR/etc/profile.d/conda.sh
+conda activate cudaq-env
+
+python scripts/cudaq_introduction.py
+# python scripts/cudaq_target.py
+# python scripts/hadamard_test.py
